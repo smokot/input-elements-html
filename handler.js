7751aac27c8bdad3540264f1cb47cd353c8f0b25
johan liebert <58879890+smokot@users.noreply.github.com>
2025-03-15 03:03:57 +0500

    Delete handler.js

--- handler.js
+++
@@ -1,8 +0,0 @@
-if(rangeElement = document.querySelector('.input-range input[type=range]')){
-    rangeElement.addEventListener('mousemove', function(){
-        let label = document.querySelector('.input-range label.value');
-        if(label){
-            label.innerText = this.value;
-        }
-    });
-}
