d7062bce6b49ab5ba23030fcf3614ea7d9a28207
johan liebert <58879890+smokot@users.noreply.github.com>
2025-03-20 21:07:27 +0500

    Update control-handler.js

--- control-handler.js
+++ control-handler.js
@@ -1,8 +1,14 @@
-if(rangeElement = document.querySelector('.input-range input[type=range]')){
-    rangeElement.addEventListener('mousemove', function(){
-        let label = document.querySelector('.input-range label.value');
-        if(label){
-            label.innerText = this.value;
+if(rangeElements = document.querySelectorAll('.input-range')){
+    rangeElements.forEach(function(rangeElement){
+        let label = rangeElement.querySelector('.input-range label.value');
+        let range = rangeElement.querySelector('input[type=range]');
+
+        if(range){
+            range.addEventListener('mousemove', function(){
+                if(label){
+                    label.innerText = this.value;
+                }
+            });
         }
     });
 }
